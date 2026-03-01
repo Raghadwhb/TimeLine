a753311f2b63823aa99093c8652850ac74db25fa
Raghadwhb <raghadwahhab.eng@gmail.com>
2026-03-01 04:24:39 +0100

    Delete TimeLine4 directory

--- TimeLine4/Config/mondb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://raghadwahhabeng_db_user:Sham.1234@cluster0.3h7ejzw.mongodb.net/timeline?appName=Cluster0')
-.then(() => {
-    console.log('Connected to MongoDB')
-})
-.catch((err) => {
-    console.error('Could not connect to MongoDB', err)
-});
